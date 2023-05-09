4fb51463753ef6374f67bb42816449ba66cc19bf
unknown <shamiullipu@gmail.com>
2023-05-10 03:59:20 +0600

    banner and services section added

--- car-doctor-client/src/Pages/Home/Home.jsx
+++ car-doctor-client/src/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import About from "./About";
+import Banner from "./Banner";
+import Services from "./Services";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Banner></Banner>
+            <About></About>
+            <Services></Services>
         </div>
     );
 };
